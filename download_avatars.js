37527c2bdf7714590b7c3b75b6743a8b05029b17
max-bergen <maxbergen1234@gmail.com>
2017-03-13 23:25:54 +0000

    can print url to console

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var GITHUB_USER = 'max-bergen';
+  var GITHUB_TOKEN = 'ad9659316c50dd84c30d6c4d50c798cbbb16c0fe'
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  console.log(requestURL);
+}
+
+
+// var GITHUB_USER = "max-bergen";
+// var GITHUB_TOKEN = "ad9659316c50dd84c30d6c4d50c798cbbb16c0fe"
+// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log('Errors:', err);
+  console.log('Result:', result);
+});
+
