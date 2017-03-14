dbfc8dda120a0ac354b99fa5e741f0415911d39c
max-bergen <maxbergen1234@gmail.com>
2017-03-14 01:32:04 +0000

    it downloads the avatars into a subdirectory called avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,17 @@
+
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = 'max-bergen';
 var GITHUB_TOKEN = 'ad9659316c50dd84c30d6c4d50c798cbbb16c0fe';
 var USER_AGENT = 'GitHub Avatar Downloader - Student Project';
 
-
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+
+    .pipe(fs.createWriteStream(filePath));
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -18,15 +25,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if (err) throw err;
     var parsedBody = JSON.parse(body);
     for (var i in parsedBody){
-    var avatarUrl = parsedBody[i].avatar_url;
-    console.log(avatarUrl);
-    }
+      var avatarUrl = parsedBody[i].avatar_url;
+      var avatarLogin = parsedBody[i].login;
+      downloadImageByURL(avatarUrl, 'avatars/' + avatarLogin + '.jpg');
+      }
   });
 };
-
-
 
 getRepoContributors('jquery', 'jquery', function(err, result){
      console.log('Errors:', err);
      console.log('Result:', result);
 });
+
+
