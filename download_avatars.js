f1dc82b0203ca2a8bc6d7d41f58d008955867524
max-bergen <maxbergen1234@gmail.com>
2017-03-14 01:10:10 +0000

    can console log the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,24 +1,32 @@
 var request = require('request');
+var GITHUB_USER = 'max-bergen';
+var GITHUB_TOKEN = 'ad9659316c50dd84c30d6c4d50c798cbbb16c0fe';
+var USER_AGENT = 'GitHub Avatar Downloader - Student Project';
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var GITHUB_USER = 'max-bergen';
-  var GITHUB_TOKEN = 'ad9659316c50dd84c30d6c4d50c798cbbb16c0fe'
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    'url': requestURL,
+    'headers': {
+      'User-Agent': USER_AGENT
+    }
+  };
+  request.get(options, function(err, response, body){
+    if (err) throw err;
+    var parsedBody = JSON.parse(body);
+    for (var i in parsedBody){
+    var avatarUrl = parsedBody[i].avatar_url;
+    console.log(avatarUrl);
+    }
+  });
+};
 
-  console.log(requestURL);
-}
 
 
-// var GITHUB_USER = "max-bergen";
-// var GITHUB_TOKEN = "ad9659316c50dd84c30d6c4d50c798cbbb16c0fe"
-// var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-
-
-getRepoContributors('jquery', 'jquery', function(err, result) {
-  console.log('Errors:', err);
-  console.log('Result:', result);
+getRepoContributors('jquery', 'jquery', function(err, result){
+     console.log('Errors:', err);
+     console.log('Result:', result);
 });
-
